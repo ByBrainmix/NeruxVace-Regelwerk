0739f5881d65f0f10a7e1a349deb8ecd25a53952
ByBrainmix <flotobias30@gmail.com>
2017-06-12 15:19:18 +0200

    add spa routing

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+page('/', function() {
+  $('#content').html($('#default').html());
+});
 
+page('/:id', function(context) {
+  var id = context.params.id;
+  load(id);
+});
+
+page.start();
+
+var converter = new showdown.Converter();
+function load(site) {
+  $('#content').load('./' + site + '.md', function(response, status, xhr) {
+    if (status === 'error') {
+      $('#content').html('<h3>Ein Fehler ist aufgetreten</h3><br><p>' + xhr.status + ' ' + xhr.statusText + '</p>');
+    } else {
+      $('#content').html(converter.makeHtml($('#content').html()));
+    }
+  });
+}
